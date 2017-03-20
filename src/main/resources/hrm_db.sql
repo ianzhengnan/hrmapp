60ccc6f7eddbb9dbb36436ec3fb627cc901e8d6d
Ian Zheng <ian.zheng@sap.com>
2017-03-20 13:36:01 +0800

    add sql file

---
+++ src/main/resources/hrm_db.sql
@@ -0,0 +1,159 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : hrm_db
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2017-03-20 11:41:58
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for dept_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `dept_inf`;
+CREATE TABLE `dept_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dept_inf
+-- ----------------------------
+INSERT INTO `dept_inf` VALUES ('1', '技术部', '技术部');
+INSERT INTO `dept_inf` VALUES ('2', '运营部', '运营部');
+INSERT INTO `dept_inf` VALUES ('3', '财务部', '财务部');
+INSERT INTO `dept_inf` VALUES ('4', '总工办', '总工办');
+INSERT INTO `dept_inf` VALUES ('5', '市场部', '市场部');
+INSERT INTO `dept_inf` VALUES ('6', '教学部', '教学部');
+
+-- ----------------------------
+-- Table structure for document_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `document_inf`;
+CREATE TABLE `document_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITLE` varchar(50) NOT NULL,
+  `FILENAME` varchar(300) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_DOCUMENT_USER` (`USER_ID`),
+  CONSTRAINT `FK_DOCUMENT_USER` FOREIGN KEY (`USER_ID`) REFERENCES `user_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of document_inf
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for employee_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `employee_inf`;
+CREATE TABLE `employee_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DEPT_ID` int(11) NOT NULL,
+  `JOB_ID` int(11) NOT NULL,
+  `NAME` varchar(20) NOT NULL,
+  `CARD_ID` varchar(18) NOT NULL,
+  `ADDRESS` varchar(50) NOT NULL,
+  `POST_CODE` varchar(50) DEFAULT NULL,
+  `TEL` varchar(16) DEFAULT NULL,
+  `PHONE` varchar(11) NOT NULL,
+  `QQ_NUM` varchar(20) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  `SEX` int(11) NOT NULL DEFAULT '1',
+  `PARTY` varchar(10) DEFAULT NULL,
+  `BIRTHDAY` datetime DEFAULT NULL,
+  `RACE` varchar(100) DEFAULT NULL,
+  `EDUCATION` varchar(10) DEFAULT NULL,
+  `SPECIALITY` varchar(20) DEFAULT NULL,
+  `HOBBY` varchar(100) DEFAULT NULL,
+  `REMARK` varchar(500) DEFAULT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  KEY `FK_EMP_DEPT` (`DEPT_ID`),
+  KEY `FK_EMP_JOB` (`JOB_ID`),
+  CONSTRAINT `FK_EMP_DEPT` FOREIGN KEY (`DEPT_ID`) REFERENCES `dept_inf` (`ID`),
+  CONSTRAINT `FK_EMP_JOB` FOREIGN KEY (`JOB_ID`) REFERENCES `job_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of employee_inf
+-- ----------------------------
+INSERT INTO `employee_inf` VALUES ('1', '1', '8', '爱丽丝', '342101198502231589', '广州天河', '510000', '020-7777777', '13761659856', '95874156', '69854756.qq.com', '0', '党员', '1982-03-20 11:35:57', '汉', '本科', '美声', '唱歌', '四大天王', '2017-03-20 11:37:02');
+INSERT INTO `employee_inf` VALUES ('2', '2', '1', '杰克', '321462198612052569', '上海浦东', '210000', '021-65989652', '13896598456', '95581714', '95847625.qq.com', '1', '党员', '1985-03-20 11:38:49', '满', '本科', '吉他', '弹吉他', 'Steve Vai', '2017-03-20 11:39:42');
+INSERT INTO `employee_inf` VALUES ('3', '1', '2', '卡卡', '369587198806092568', '上海嘉定', '210000', '021-56985847', '13695847562', '5698547', '69584723.qq.com', '1', '群众', '1988-03-20 11:40:57', '汉', '大专', '踢球', '踢球', '卡卡', '2017-03-20 11:41:25');
+
+-- ----------------------------
+-- Table structure for job_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `job_inf`;
+CREATE TABLE `job_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  `REMARK` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of job_inf
+-- ----------------------------
+INSERT INTO `job_inf` VALUES ('1', '职员', '职员');
+INSERT INTO `job_inf` VALUES ('2', 'Java开发工程师', 'Java开发工程师');
+INSERT INTO `job_inf` VALUES ('3', 'Java开发中级工程师', 'Java开发中级工程师');
+INSERT INTO `job_inf` VALUES ('4', 'Java开发高级工程师', 'Java开发高级工程师');
+INSERT INTO `job_inf` VALUES ('5', '系统管理员', '系统管理员');
+INSERT INTO `job_inf` VALUES ('6', '架构师', '架构师');
+INSERT INTO `job_inf` VALUES ('7', '主管', '主管');
+INSERT INTO `job_inf` VALUES ('8', '经理', '经理');
+INSERT INTO `job_inf` VALUES ('9', '总经理', '总经理');
+
+-- ----------------------------
+-- Table structure for notice_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `notice_inf`;
+CREATE TABLE `notice_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TITILE` varchar(50) NOT NULL,
+  `CONTENT` text NOT NULL,
+  `CREATE_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_NOTICE_USER` (`USER_ID`),
+  CONSTRAINT `FK_NOTICE_USER` FOREIGN KEY (`USER_ID`) REFERENCES `user_inf` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of notice_inf
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_inf
+-- ----------------------------
+DROP TABLE IF EXISTS `user_inf`;
+CREATE TABLE `user_inf` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LOGINNAME` varchar(20) NOT NULL,
+  `PASSWORD` varchar(16) NOT NULL,
+  `STATUS` int(11) NOT NULL DEFAULT '1',
+  `CREATEDATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USERNAME` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_inf
+-- ----------------------------
+INSERT INTO `user_inf` VALUES ('1', 'admin', '1234567', '2', '2017-03-20 11:32:38', '超级管理员');
+INSERT INTO `user_inf` VALUES ('2', 'kaka', '1234567', '1', '2017-03-20 11:33:21', '普通管理员');
